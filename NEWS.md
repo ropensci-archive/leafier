55661980dadfeaf5ae978391d8e79a1d09d02e03
Jonathan Carroll <jono@jcarroll.com.au>
2016-05-18 21:50:53 +0930

    added leafier-package.R whole package documentation, incremented version, added myself as contributor (I'd love to help out).

--- NEWS.md
+++ NEWS.md
@@ -1,2 +1,6 @@
+# leafier 0.0.2
+* Initial package commit
+  - minor modifications by J. Carroll to bring build back online.
+
 # leafier 0.0.1
 * Initial package commit
---
+++ R/leafier-package.R
@@ -0,0 +1,13 @@
+#' leafier: Render large spatial datasets in leaflet
+#'
+#' Uses smart analysis to easily plot the visible data at an efficient scale.
+#'
+#' @section processing functions:
+#' The processing functions ...
+#'
+#' @section plotting functions:
+#' The plotting functions ...
+#'
+#' @docType package
+#' @name leafier
+NULL
